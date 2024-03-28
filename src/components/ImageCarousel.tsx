0cb4bbc68ca181a1ab6edd82c7a9080d7b37e1b1
DanielSan1990 <danielsla90@gmail.com>
2024-03-28 21:17:16 +0000

    In progress

---
+++ src/components/ImageCarousel.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'preact/hooks';
+
+export default function ImageCarousel({ images }: any) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const nextImage = () => setCurrentIndex((currentIndex + 1) % images.length);
+  const prevImage = () => setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+
+  return (
+    <div class="space-y-2">
+      <div class="relative w-full h-96 mx-auto overflow-hidden rounded-lg">
+        <img src={images[currentIndex]} alt={`Imagen ${currentIndex + 1}`} class="w-full h-full object-cover" />
+        <button
+          class="absolute top-1/2 left-0 transform -translate-y-1/2 p-2"
+          onClick={prevImage}
+        >
+          <i class="fa-solid fa-arrow-left"></i>
+        </button>
+        <button
+          class="absolute top-1/2 right-0 transform -translate-y-1/2 p-2"
+          onClick={nextImage}
+        >
+          <i class="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+      <div class="flex justify-center space-x-2 mt-4">
+        {images.map((_: any, index: number): any => (
+          <button
+            key={index}
+            class={`h-4 w-4 rounded-full ${currentIndex === index ? 'bg-green-700' : 'bg-gray-300'}`}
+            aria-label={`Ir a la imagen ${index + 1}`}
+            onClick={() => setCurrentIndex(index)}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+}
