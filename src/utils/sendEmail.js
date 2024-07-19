a6ca208cfe159b1933215ac4bf225652cc80172b
Daniel San Luis <daniel.sanluis@panel.es>
2024-07-19 20:33:49 +0100

    ADD MAILTO

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); // Evita el envío del formulario
+
+      // Obtiene los valores del formulario
+      const name = form.name.value;
+      const email = form.email.value;
+      const subject = form.subject.value;
+      const message = form.message.value;
+
+      // Construye el enlace mailto
+      const mailtoLink = `mailto:destino@correo.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(`Nombre: ${name}\nCorreo electrónico: ${email}\n\nMensaje:\n${message}`)}`;
+
+      // Abre el enlace mailto
+      window.location.href = mailtoLink;
+    });
+  });
